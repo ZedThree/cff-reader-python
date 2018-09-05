7e9bdf39cf7004b08704061f725eb222b5440293
Peter Hill <zed.three@gmail.com>
2018-09-05 13:47:53 +0100

    Add method for downloading schema

--- cffreader/reader.py
+++ cffreader/reader.py
@@ -1,8 +1,14 @@
 import re
+import requests
 import yaml
 import sys
 import contextlib
 import requests
+
+
+CFF_CORE_SCHEMA_URL = ("https://raw.githubusercontent.com/citation-file-format"
+                       "/schema/{0}/CFF-Core/schema.yaml")
+
 
 @contextlib.contextmanager
 def smart_open(filename, mode='Ur'):
@@ -25,25 +31,25 @@ def smart_open(filename, mode='Ur'):
 
 def reader(from_filename=None, from_file=None,from_url=None):
     ''' Reads a cff file from filename, open file, stdin (specify from_filename='-') or url and returns a dictionary.'''
-    
+
     #Need to first get the input and turn it into a string.
-    
+
     if from_filename:
         with smart_open(from_filename, 'r') as f:
             cffstr = f.read()
-            
+
     elif from_file:
         if isinstance(from_file, str):
             raise ValueError("Please pass a filename using argument from_filename.")
-        
+
         cffstr = from_file.read()
-        
+
     elif from_url:
         if from_url.startswith("https://github.com"):
             #We know how to handle github urls so can continue
             #URL could be a link to a repository or the citation file
             baseurl = "https://raw.githubusercontent.com"
-            
+
             regexp = re.compile("^" +
                                 "(?P<baseurl>https://github\.com)/" +
                                 "(?P<org>[^/\n]*)/" +
@@ -68,15 +74,15 @@ def reader(from_filename=None, from_file=None,from_url=None):
                 cffstr = r.text
             else:
                 raise Exception("Error requesting file: {0}".format(file_url))
-            
+
         else:
             raise Exception("Only 'https://github.com' URLs are supported at the moment.")
-        
+
     else:
         raise ValueError("You should specify the file/filename to read, url to read from or stdin")
 
 
-    cffdict = yaml.safe_load(cffstr)    
+    cffdict = yaml.safe_load(cffstr)
     if not isinstance(cffdict, dict):
         raise ValueError("Provided CITATION.cff does not seem valid YAML.")
 
@@ -111,8 +117,22 @@ class CFFfile:
 
         return semver
 
+    def get_schema(self):
+        """Get the schema for the version of the file
+
+        """
+        if hasattr(self, "_schema") and self._schema is not None:
+            return self._schema
+
+        semver = self.get_version()
+        r = requests.get(CFF_CORE_SCHEMA_URL.format(semver))
+        r.raise_for_status()
+        return r.text
+
+    def validate(self):
+        pass
+
 
 if __name__ == "__main__":
     cffdict = reader(from_filename="../CITATION.cff")
     print(cffdict)
-    
