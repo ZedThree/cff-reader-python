5046bd4119e3d409b363ce88f86f542e121a7e4e
Jennifer C J Radtke <jcj.radtke@dunelm.org.uk>
2018-09-05 15:29:39 +0100

    Add reader functionality for files, urls and stdin.

--- cffreader/reader.py
+++ cffreader/reader.py
@@ -1,15 +1,86 @@
 import re
 import yaml
+import sys
+import contextlib
+import requests
+
+@contextlib.contextmanager
+def smart_open(filename, mode='Ur'):
+    """Open stdin or stdout using a contextmanager
+    From: http://stackoverflow.com/a/29824059/2043465
+    """
+    if filename == '-':
+        if mode is None or mode == '' or 'r' in mode:
+            fh = sys.stdin
+        else:
+            fh = sys.stdout
+    else:
+        fh = open(filename, mode)
+    try:
+        yield fh
+    finally:
+        if filename is not '-':
+            fh.close()
 
 
-class Reader:
-    def __init__(self, url=None, filename=None):
-        # Either get the file from url
-        # Or open and read filename
+def reader(from_filename=None, from_file=None,from_url=None):
+    ''' Reads a cff file from filename, open file, stdin (specify from_filename='-') or url and returns a dictionary.'''
+    
+    #Need to first get the input and turn it into a string.
+    
+    if from_filename:
+        with smart_open(from_filename, 'r') as f:
+            cffstr = f.read()
+            
+    elif from_file:
+        if isinstance(from_file, str):
+            raise ValueError("Please pass a filename using argument from_filename.")
+        
+        cffstr = from_file.read()
+        
+    elif from_url:
+        if from_url.startswith("https://github.com"):
+            #We know how to handle github urls so can continue
+            #URL could be a link to a repository or the citation file
+            baseurl = "https://raw.githubusercontent.com"
+            
+            regexp = re.compile("^" +
+                                "(?P<baseurl>https://github\.com)/" +
+                                "(?P<org>[^/\n]*)/" +
+                                "(?P<repo>[^/\n]*)" +
+                                "(/tree/(?P<label>[^/\n]*))?", re.IGNORECASE)
 
-        # Create and return CFFfile
-        pass
+            matched = re.match(regexp, from_url)
+            if matched is None:
+                raise Exception("Error extracting (user|organization) and/or repository " +
+                                "information from the provided URL ({0}).".format(from_url))
+            else:
+                url_parts = matched.groupdict()
 
+            file_url = "/".join([baseurl,
+                                url_parts["org"],
+                                url_parts["repo"],
+                                url_parts["label"] if url_parts["label"] is not None else "master",
+                                "CITATION.cff"])
+
+            r = requests.get(file_url)
+            if r.ok:
+                cffstr = r.text
+            else:
+                raise Exception("Error requesting file: {0}".format(file_url))
+            
+        else:
+            raise Exception("Only 'https://github.com' URLs are supported at the moment.")
+        
+    else:
+        raise ValueError("You should specify the file/filename to read, url to read from or stdin")
+
+
+    cffdict = yaml.safe_load(cffstr)    
+    if not isinstance(cffdict, dict):
+        raise ValueError("Provided CITATION.cff does not seem valid YAML.")
+
+    return cffdict
 
 class CFFfile:
     def __init__(self, text):
@@ -39,3 +110,9 @@ class CFFfile:
             raise ValueError("Unable to identify the schema version. Does the CFF include the 'cff-version' key?")
 
         return semver
+
+
+if __name__ == "__main__":
+    cffdict = reader(from_filename="../CITATION.cff")
+    print(cffdict)
+    
